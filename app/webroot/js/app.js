599def152545ddaf53fd2c42feae90cd0c98b1d2
lakhansamani <lakhan.m.samani@gmail.com>
2015-06-03 13:43:09 +0530

    updates student layout with custom dropdown

--- app/webroot/js/app.js
+++ app/webroot/js/app.js
@@ -12,3 +12,9 @@ $(".cs-dropdown").mouseover(function(){
 	$(".course-menu > ul > .active").children(".subject-menu").show();
 	$(".course-menu > ul >").removeClass("active");
 })
+$(".has-sub-menu").mouseover(function(){
+	$(this).children(".sub-menu").show();
+});
+$(".has-sub-menu").mouseout(function(){
+	$(this).children(".sub-menu").hide();
+})
